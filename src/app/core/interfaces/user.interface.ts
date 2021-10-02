0001d1656ecf73b7638ad48420480ee45d221af7
Sokolovskiy <antiandru5932@gmail.com>
2021-10-02 23:32:14 +0300

    add edit, delete, add user actions

--- src/app/core/interfaces/user.interface.ts
+++ src/app/core/interfaces/user.interface.ts
@@ -2,7 +2,7 @@ export interface User {
   id: number;
   firstName: string;
   lastName: string;
-  role: "Riskmanager" | "Processexpert" | "Admin" | "Auditor";
+  role: 'Riskmanager' | 'Processexpert' | 'Admin' | 'Auditor';
   email: string;
   birthDate: Date;
 }
--- src/app/store/user/user.actions.ts
+++ src/app/store/user/user.actions.ts
@@ -3,8 +3,22 @@ import { User } from '../../core/interfaces';
 import { UserActionsEnum } from '../../core/enums';
 
 export const getUserList = createAction(UserActionsEnum.GetUserList);
-
 export const getUserListSuccess = createAction(
   UserActionsEnum.GetUserListSuccess,
   props<{ userList: User[] }>()
+);
+
+export const addUser = createAction(
+  UserActionsEnum.AddUser,
+  props<{ userInfo: User }>()
+);
+
+export const deleteUser = createAction(
+  UserActionsEnum.DeleteUser,
+  props<{ userId: number }>()
+);
+
+export const editUser = createAction(
+  UserActionsEnum.EditUser,
+  props<{ userInfo: User }>()
 );
